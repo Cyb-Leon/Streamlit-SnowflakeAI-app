f2a6768a296451d15ac4a77734f93fcd156834e4
Cyb-Leon <zamokuhlendaba@gmail.com>
2026-01-07 22:24:05 +0200

    restructured files and renaming for multipage display via Streamlit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import streamlit as st
+
+# Connect to Snowflake
+try:
+    # Works in Streamlit in Snowflake
+    from snowflake.snowpark.context import get_active_session
+    session = get_active_session()
+except:
+    # Works locally and on Streamlit Community Cloud
+    from snowflake.snowpark import Session
+    session = Session.builder.configs(st.secrets["connections"]["snowflake"]).create()
