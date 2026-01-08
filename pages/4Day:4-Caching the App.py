8d2c47f4f38b935460e78e91982914ec94a855e0
Cyb-Leon <zamokuhlendaba@gmail.com>
2026-01-08 09:01:39 +0200

    multipage with page folder strategy

--- pages/4Day:4-Caching the App.py
+++ pages/4Day:4-Caching the App.py
@@ -6,6 +6,15 @@ from snowflake.snowpark.functions import ai_complete
 st.title("Hi i am stupid")
 
 st.title(":material/cached: Caching your App")
+# Connect to Snowflake
+try:
+    # Works in Streamlit in Snowflake
+    from snowflake.snowpark.context import get_active_session
+    session = get_active_session()
+except:
+    # Works locally and on Streamlit Community Cloud
+    from snowflake.snowpark import Session
+    session = Session.builder.configs(st.secrets["connections"]["snowflake"]).create()
 
 
 @st.cache_data
