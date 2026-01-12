a0c8f18a9b8a8278d8b6dee5e9d1ba5c9757ed4f
Cyb-Leon <zamokuhlendaba@gmail.com>
2026-01-13 00:59:04 +0200

    inline links - common practice

--- Home Menu Challenges.py
+++ Home Menu Challenges.py
@@ -8,24 +8,24 @@ col1.write("Beingnner levels")
 
 with col1:
     st.badge("Mortal", color="green")
-    st.page_link("pages/1Day:1- Connect To Snowflake")
-    st.page_link("pages/2Day:2- Run LLM (Cortex AI ) In Snowflake ")
-    st.page_link("pages/6Day:6- Status UI for Long-Running Task")
+    st.page_link("pages/1Day:1- Connect To Snowflake.py")
+    st.page_link("pages/2Day:2- Run LLM (Cortex AI ) In Snowflake.py")
+    st.page_link("pages/6Day:6- Status UI for Long-Running Task.py")
 
 col2.write("Intermedate levels")
 
 with col2:
     st.badge("Hero", color="yellow")
-    st.page_link("pages/3Day:3- Write streams")
-    st.page_link("pages/4Day:4- Caching the App")
-    st.page_link("pages/5Day:5- Post Generator App")
-    st.page_link("pages/8Day:8- Meet the Chat Elements")
+    st.page_link("pages/3Day:3- Write streams.py")
+    st.page_link("pages/4Day:4- Caching the App.py")
+    st.page_link("pages/5Day:5- Post Generator App.py")
+    st.page_link("pages/8Day:8- Meet the Chat Elements.py")
 col3.write("Advanced levels")
 with col3:
     st.badge("Spartan", color="red")
-    st.page_link("pages/7Day:7- Theming and Layout")
-    st.page_link("pages/10Day:10- StreamlitxSnowflake Chatbot")
-    st.page_link("pages/9Day:9- Understanding Session State")
+    st.page_link("pages/7Day:7- Theming and Layout.py")
+    st.page_link("pages/10Day:10- StreamlitxSnowflake Chatbot.py")
+    st.page_link("pages/9Day:9- Understanding Session State.py")
     st.page_link("pages/11Day:11- Displaying Chat History.py")
 
 
