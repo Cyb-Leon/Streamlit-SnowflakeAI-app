f331d8d0433fda8e2be0c6bc4581713aace8897d
Cyb-Leon <zamokuhlendaba@gmail.com>
2026-01-26 11:53:49 +0200

    update syntax

--- pages/24Day 24: Working with Images (Multimodality).py
+++ pages/24Day 24: Working with Images (Multimodality).py
@@ -94,7 +94,7 @@ with st.container(border=True):
         col1, col2 = st.columns(2)
         
         with col1:
-            st.image(uploaded_file, caption="Your Image", use_container_width=True)
+            st.image(uploaded_file, caption="Your Image", width=stretch)
         
         with col2:
             st.write(f"**File:** {uploaded_file.name}")
