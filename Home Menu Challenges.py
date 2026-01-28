ac9574b3d2bb28545fe6d0a7458647789c5fd661
Cyb-Leon <zamokuhlendaba@gmail.com>
2026-01-28 17:46:15 +0200

    update with day 26

--- Home Menu Challenges.py
+++ Home Menu Challenges.py
@@ -29,6 +29,7 @@ with col2:
     st.page_link("pages/21Day 21: RAG with Cortex Search.py")
     st.page_link("pages/22Day 22: Chat with Your Documents.py")
     st.page_link("pages/24Day 24: Working with Images (Multimodality).py")
+    st.page_link("pages/26Day 26: Introduction to Cortex Agents | Create Your First Agent.py")
 
 col3.write("Advanced levels")
 with col3:
